3256d39a297ec15a6a547aaa15f1d0ac7cdd095b
shinyaigeek <PhilisPaxil@gmail.com>
2020-09-25 22:56:16 +0900

    add: BundleSize Analyzer

--- packages/analyzer/main.ts
+++ packages/analyzer/main.ts
@@ -1,35 +1,6 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import { getBundleSize } from "./lib/getBundleSize/main";
+import { TARGET } from "./vars";
 
-module.exports = {
-  entry: {
-    bundle: "./src/App.tsx"
-  },
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "[name].js"
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "dist")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        loader: "ts-loader"
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ]
-};
+TARGET.forEach((pk) => {
+    console.log(getBundleSize(pk) + "B")
+})
